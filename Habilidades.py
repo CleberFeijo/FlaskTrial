e17a01e7aae434a4f56dd4e1e66dde79ddccc8b4
CleberFeijo <43549597+CleberFeijo@users.noreply.github.com>
2021-04-22 22:26:51 -0300

    Update Habilidades.py

--- Habilidades.py
+++ Habilidades.py
@@ -1,6 +1,31 @@
 from flask_restful import Resource
+from flask import request
+import json
 
 lista_habilidades = ['Python', 'Java', 'PhP', 'Flask', 'Ruby']
 class Habilidades(Resource):
     def get(self):
+        return lista_habilidades
+    def post(self):
+        hab = json.loads(request.data)
+        lista_habilidades.append(hab)
+        return(lista_habilidades)
+
+class Habilidade(Resource):
+    def get(self, id):
+        try:
+            response = lista_habilidades[id]
+        except IndexError:
+            mensagem = 'Desenvolvedor de ID {} nao existe'.format(id)
+            response = {'status': 'erro', 'mensagem': mensagem}
+        except Exception:
+            mensagem = 'Erro desconhecido. Procure o adm da API'
+            response = {'status': 'Erro', 'mensagem': mensagem}
+        return response
+    def put(self, id):
+        hab = json.loads(request.data)
+        lista_habilidades[id] = hab
+        return lista_habilidades
+    def delete(self, id):
+        lista_habilidades.pop(id)
         return lista_habilidades
